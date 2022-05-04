b174ac56b3bfd9c96bf5720cf5b3e58fa2d0dc4c
Ariel Pellegrino Iborra <ariel.iborra@sensedia.com>
2022-05-04 10:51:58 -0300

    comitando alterações de repositorio

---
+++ Calculadora - IMC/imc.js
@@ -0,0 +1,7 @@
+
+const nome = document.getElementById('nome');
+const altura = document.getElementById('altura');
+const peso = document.getElementById('peso');
+const calcular = document.getElementById('calcular');
+const resultado = document.getElementById('resultado');
+
